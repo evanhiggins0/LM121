25a415d83c34840dd34a27f6db22b87082a5e06b
evanhiggins0 <54105097+evanhiggins0@users.noreply.github.com>
2020-12-02 11:34:04 +0000

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,15 +1,18 @@
 // Evan Higgins
 // One part of my submission for Assignment 3 of Intro to Programming, which was to build a class that would create a "contact list" of information for a person, similar to that on a phone.
 // We had to follow specific guidelines in relation to formatting. 
-public class Contact{
 
+public class Contact{
+	
+	// private string variables to contain input data from objects
     private String contactName;
     private String phoneNumber; 
     private String emailAddress;
     private String facebookHandle; 
     private String instagramHandle; 
     private String twitterHandle;
-
+	
+	// public "constant" string variables
     public String nameList = "Name";
     public String numList = "Phone Number";
     public String emailList = "Email Address";
@@ -17,10 +20,11 @@ public class Contact{
     public String igList = "Instagram Handle";
     public String twList = "Twitter Handle";
 
+	// setting up for object usage
     public Contact(String name){
         contactName = name;
     }
-
+    
     public Contact(String name, String number){
         contactName = name;
         phoneNumber = number;
@@ -47,15 +51,15 @@ public class Contact{
         instagramHandle = instagram;
         twitterHandle = twitter;
     }
-
+	// returns the current variable value
     public String getContactName(){
         return contactName;
     }
-
+	// sets the variable value
     public void setContactName(String name){
         contactName = name;
     }
-
+    // rinse and repeat
     public String getPhoneNumber(){
         return phoneNumber;
     }
@@ -95,11 +99,11 @@ public class Contact{
     public void setTwitterHandle(String twitter){
         twitterHandle = twitter;
     }
-
+	// method to centre "input" text, ie. second column. 
     public static String centre(String text){
         if(text.length() > 30){
             text = (text.substring(0, 27) + "...\n");
-        }else if(text == ""){
+        }else if(text == ""){ // if user input is blank, we do not add any spaces.
             return text;
         }else{
             int width = 30;
@@ -111,22 +115,22 @@ public class Contact{
         }
         return text; 
     }
-
+	// method to expand "contant" text, ie. first column
     public static String expand(String text){
         int n = 17 - text.length();
-        if(text != ""){
+        if(text != ""){ // checks if text isn't blank, if so expands
             for(int i = 0; i < n; i++){
                 text = text + " ";
             }
         }else if(text.length() > 17){
             return text.substring(0, 17);
         }
-        if(text != ""){
+        if(text != ""){// checks if text isn't blank, if so appends ": " to end
             text = text + ": ";
         }
         return text;  
     }
-
+	// checker which verifies the length of the corresponding "input" for the "contant", and if there is no user input for object, does not print the corresponding contant
     public String change(String text){
         if(contactName  == ""){
             nameList = "";
@@ -148,7 +152,7 @@ public class Contact{
         }
         return text;
     }
-
+	// creates desired format
     public String toString(){
         return String.format(change(expand(nameList)) + centre(contactName) + change(expand(numList)) + centre(phoneNumber) + 
             change(expand(emailList)) + centre(emailAddress) + change(expand(fbList)) + centre(facebookHandle) + change(expand(igList)) 
